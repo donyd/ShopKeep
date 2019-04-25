4462e67053c22f25202326d33e4713966ef08226
Donal D'silva <donal_dsilva@hotmail.com>
2019-04-25 20:53:33 +0100

    added mobile vision overlay and helpers

---
+++ app/src/main/java/com/donyd/jsunscripted/www/shopkeep/OcrDetectorProcessor.java
@@ -0,0 +1,47 @@
+package com.donyd.jsunscripted.www.shopkeep;
+
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.donyd.jsunscripted.www.shopkeep.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+/**
+ * A very simple Processor which gets detected TextBlocks and adds them to the overlay
+ * as OcrGraphics.
+ * TODO: Make this implement Detector.Processor<TextBlock> and add text to the GraphicOverlay
+ */
+public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
+
+    private GraphicOverlay<OcrGraphic> graphicOverlay;
+
+    OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
+        graphicOverlay = ocrGraphicOverlay;
+    }
+
+
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        graphicOverlay.clear();
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            if(item != null && item.getValue() != null){
+                Log.d("Processor", "Text detected! " + item.getValue());
+                OcrGraphic graphic = new OcrGraphic(graphicOverlay, item);
+                graphicOverlay.add(graphic);
+            }
+        }
+
+    }
+
+    @Override
+    public void release() {
+        graphicOverlay.clear();
+
+    }
+
+    // TODO:  Once this implements Detector.Processor<TextBlock>, implement the abstract methods.
+}
+
