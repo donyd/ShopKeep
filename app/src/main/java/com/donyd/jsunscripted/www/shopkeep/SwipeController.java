62e462827ca12b2cc1431920ec19637588af2a7b
Donal D'silva <donal_dsilva@hotmail.com>
2019-05-08 12:55:50 +0100

    swipe logic fixed

--- app/src/main/java/com/donyd/jsunscripted/www/shopkeep/SwipeController.java
+++ app/src/main/java/com/donyd/jsunscripted/www/shopkeep/SwipeController.java
@@ -26,6 +26,7 @@ public class SwipeController extends Callback {
     // global variables
     private boolean swipeBack = false;
     private ButtonsState buttonShowedState = ButtonsState.GONE;
+    private RecyclerView.ViewHolder currentItemViewHolder = null;
     private static final float buttonWidth = 150;
 
     private RectF buttonInstance = null;
@@ -52,7 +53,8 @@ public class SwipeController extends Callback {
     @Override
     public int convertToAbsoluteDirection(int flags, int layoutDirection){
         if (swipeBack){
-            swipeBack = false;
+            // swipeBack = false;
+            swipeBack = buttonShowedState != ButtonsState.GONE;
             return 0;
         }
         return super.convertToAbsoluteDirection(flags, layoutDirection);
@@ -62,13 +64,21 @@ public class SwipeController extends Callback {
     @Override
     public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
                             float dX, float dY, int actionState, boolean isCurrentlyActive){
-        if (actionState == ACTION_STATE_SWIPE){
-            setTouchListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        if (actionState == ACTION_STATE_SWIPE) {
+            if (buttonShowedState != ButtonsState.GONE) {
+                if (buttonShowedState == ButtonsState.LEFT_VISIBLE) dX = Math.max(dX, buttonWidth);
+                if (buttonShowedState == ButtonsState.RIGHT_VISIBLE) dX = Math.min(dX, -buttonWidth);
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+            else {
+                setTouchListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
         }
 
-        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-
-        drawButtons(c, viewHolder);
+        if (buttonShowedState == ButtonsState.GONE) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+        currentItemViewHolder = viewHolder;
 
     }
 
@@ -118,7 +128,7 @@ public class SwipeController extends Callback {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_UP){
-                    SwipeController.super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                    SwipeController.super.onChildDraw(c, recyclerView, viewHolder, 0F, dY, actionState, isCurrentlyActive);
                     recyclerView.setOnTouchListener(new View.OnTouchListener(){
                         @Override
                         public boolean onTouch(View v, MotionEvent event){
@@ -135,7 +145,7 @@ public class SwipeController extends Callback {
     } // eof setTouchUpListener
 
     private void setItemsClickable(RecyclerView recyclerView, boolean isClickable) {
-        for (int i = 0; i < recyclerView.getChildCount(); ++i){
+        for (int i = 0; i < recyclerView.getChildCount(); i++){
             recyclerView.getChildAt(i).setClickable(isClickable);
         }
     } // eof setItemsClickable
