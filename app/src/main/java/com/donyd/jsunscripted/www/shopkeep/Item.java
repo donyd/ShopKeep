185c7fc46e9efd9be290651cf11ee096beb92981
Donal D'silva <donal_dsilva@hotmail.com>
2019-05-13 21:12:01 +0100

    database logic added

--- app/src/main/java/com/donyd/jsunscripted/www/shopkeep/Item.java
+++ app/src/main/java/com/donyd/jsunscripted/www/shopkeep/Item.java
@@ -7,12 +7,15 @@ import java.io.Serializable;
 public class Item implements Serializable{
     private String name;
     private float price;
+    private int id;
 
     // Getters and Setters
+    public int getId(){ return id; }
+    public void setId(int id) { this.id = id;}
+
     public String getName() {
         return name;
     }
-
     public void setName(String name){
         this.name = name;
     }
@@ -20,9 +23,7 @@ public class Item implements Serializable{
     public String getPrice() {
         return Float.toString(price);
     }
-
     public float getFloatPrice() { return price; }
-
     public void setPrice(float price){
         this.price = price;
     }
@@ -30,6 +31,20 @@ public class Item implements Serializable{
     public Item(String name, float price){
         this.name = name;
         this.price = price;
+    }
+
+    public Item(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    // code adapted from https://coderanch.com/t/561362/java/compare-object-exists-arraylist-add
+    @Override
+    public boolean equals(Object obj){
+
+        Item compareItem = (Item) obj;
+
+        return this.name.equals(compareItem.name);
     }
 
 }
--- app/src/main/java/com/donyd/jsunscripted/www/shopkeep/sqlite/DatabaseHelper.java
+++ app/src/main/java/com/donyd/jsunscripted/www/shopkeep/sqlite/DatabaseHelper.java
@@ -2,8 +2,10 @@ package com.donyd.jsunscripted.www.shopkeep.sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.donyd.jsunscripted.www.shopkeep.Item;
 import com.donyd.jsunscripted.www.shopkeep.sqlite.ShopKeepContract.Products;
@@ -21,6 +23,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "ShopKeep.db";
+
+    public ArrayList<Item> shoppingList = new ArrayList<>();
 
     public DatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -62,14 +66,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues =  new ContentValues();
 
+        // get current list of products if any
+        ArrayList<Item> products = getExistingProducts();
+
         // Iterator, code adapted from https://www.tutorialspoint.com/iterate-through-arraylist-in-java
         Iterator listLooper = shoppingList.iterator();
+
         while (listLooper.hasNext()){
-            // contentValues.put(Purchases.COLUMN_PRODUCT_ID, 1);
-            contentValues.put(Purchases.COLUMN_PURCHASE_DATE, getDateTime());
-            contentValues.put(Purchases.COLUMN_ESTIMATED_DURATION, getDateTime());
-            contentValues.put(Purchases.COLUMN_ACTUAL_DURATION, getDateTime());
-        }
+            // Loop through products
+            // cast to a variable in order to prevent probable skipping
+            Item item = (Item) listLooper.next();
+
+            for (int i = 0; i < products.size(); i++){
+
+                if(products.get(i).equals(item)){ // if a product already exists update purchases table
+                    contentValues.put(Purchases.COLUMN_PRODUCT_ID, getProdIdByName(item.getName()));
+                    contentValues.put(Purchases.COLUMN_PURCHASE_DATE, getDateTime());
+                    contentValues.put(Purchases.COLUMN_ESTIMATED_DURATION, getDateTime());
+                    contentValues.put(Purchases.COLUMN_ACTUAL_DURATION, getDateTime());
+
+                } else { // update product table as well
+                    // Product Table
+                    contentValues.put(Products.COLUMN_NAME, item.getName());
+                    contentValues.put(Products.COLUMN_PRICE, item.getFloatPrice());
+
+                    // Purchases Table
+                    contentValues.put(Purchases.COLUMN_PRODUCT_ID, getProdIdByName(item.getName()));
+                    contentValues.put(Purchases.COLUMN_PURCHASE_DATE, getDateTime());
+                    contentValues.put(Purchases.COLUMN_ESTIMATED_DURATION, getDateTime());
+                    contentValues.put(Purchases.COLUMN_ACTUAL_DURATION, getDateTime());
+
+                } // else
+            } // for
+        } // while
 
         long result = db.insert(Purchases.TABLE_NAME, null, contentValues);
 
@@ -81,6 +110,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+
+    // Query string to retrieve product _ID by name
+    private int getProdIdByName(String name){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String queryString = "SELECT " + Products._ID + " FROM "
+                + Products.TABLE_NAME + " WHERE " + Products.COLUMN_NAME + " = " + name;
+
+        Cursor productId = db.rawQuery(queryString, null);
+
+        // Close cursor resource
+        productId.close();
+        return productId.getInt(0);
+    }
+
+
     // code adapted from https://tips.androidhive.info/2013/10/android-insert-datetime-value-in-sqlite-database/
     private String getDateTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat(
@@ -88,7 +133,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         Date date = new Date();
         return dateFormat.format(date);
-    }
+
+    } // eof getDateTime
+
 
     // TODO: Implement logic for shoppingList capture
     // 1. Take first item from the list
@@ -98,5 +145,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //    then insert item into product table
     // 4. Insert item into purchases table with purchases.product_id = product.id
 
+    // Helper method to retrieve all products table records
+    // code adapted from https://app.pluralsight.com/player?course=android-database-application-sqlite-building-your-first&author=simone-alessandria&name=android-database-application-sqlite-building-your-first-m3&clip=6&mode=live
+    // also previously utilized here https://github.com/donyd/FitnessAppPrototype/blob/master/app/src/main/java/com/unscripted/www/fitnessappprototype/WorkoutActivity.java
+    private ArrayList<Item> getExistingProducts(){
+        // Get instance of database
+        SQLiteDatabase db = this.getReadableDatabase();
 
-}
+        // Query string to retrieve all products
+        String selectProductsQuery = "SELECT " + Products._ID + ", " + Products.COLUMN_NAME + " FROM "
+                + Products.TABLE_NAME;
+
+        // Retrieve records in a Cursor set object
+        Cursor productSet = db.rawQuery(selectProductsQuery, null);
+
+        // Verifying query return
+        int i = productSet.getCount();
+        Log.d("Record Count", String.valueOf(i));
+
+        // Iterate through returned cursor set and
+        // populate items with values
+        while(productSet.moveToNext()){
+            // retrieve columns _ID and name from each row and add to item
+            // and https://developer.android.com/reference/android/database/Cursor
+            Item currItem = new Item(productSet.getInt(0), productSet.getString(2));
+            // add item to
+            shoppingList.add(currItem);
+
+        }
+
+        // Close resources
+        productSet.close();
+        return shoppingList;
+
+    } // eof readData
+
+} // eof Class File
